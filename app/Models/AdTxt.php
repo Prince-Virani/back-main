e7154f78ed8b501272f09ad6458119ddf44cd6fa
devOps-pri <princevirani8822@gmail.com>
2026-04-07 15:47:38 +0530

    1chang-adtxt

--- app/Models/AdTxt.php
+++ app/Models/AdTxt.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Adtxt extends Model
+class AdTxt extends Model
 {
     use HasFactory;
 
